0cfab05d6421460ddffcd6d0c668d204a11ac313
evpozdniakov <evpozdniakov@gmail.com>
2019-04-15 23:08:05 -0400

    Add FieldCheckbox

---
+++ src/components/common/FieldCheckbox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="field">
+    <label class="checkbox">
+      <input
+        v-model="isChecked"
+        type="checkbox"
+        class="checkbox"
+      >
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isChecked: this.value,
+    }
+  },
+  watch: {
+    isChecked() {
+      this.$emit('input', this.isChecked)
+    },
+  },
+};
+</script>
+
+<style>
+</style>
