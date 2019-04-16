4537ecddda333c2a0df680094d66f9a3c7466507
evpozdniakov <evpozdniakov@gmail.com>
2019-04-15 22:28:35 -0400

    Add FieldText.vue

---
+++ src/components/common/FieldText.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="field">
+    <label class="label">{{ label }}:</label>
+    <div class="control">
+      <input
+        v-model="value"
+        class="input"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FieldText',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
