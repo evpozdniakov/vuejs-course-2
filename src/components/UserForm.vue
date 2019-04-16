d7372b486ff6766d8f5e003386b58e49252949f6
evpozdniakov <evpozdniakov@gmail.com>
2019-04-15 23:09:01 -0400

    Add UserForm and use it in UserProfile

---
+++ src/components/UserForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <form class="user-form">
+    <field-text
+      v-model="user.firstName"
+      label="First name"
+    />
+    <field-text
+      v-model="user.lastName"
+      label="Last name"
+    />
+    <field-text
+      v-model="user.email"
+      label="Email"
+      type="email"
+    />
+    <field-text
+      v-model="user.phone"
+      label="Phone"
+      type="phone"
+    />
+    <field-text
+      v-model="user.address"
+      label="Address"
+    />
+    <field-checkbox
+      v-model="user.isActive"
+      label="Active"
+    />
+  </form>
+</template>
+
+<script>
+import FieldText from '@/components/common/FieldText.vue';
+import FieldCheckbox from '@/components/common/FieldCheckbox.vue';
+
+export default {
+  name: 'UserForm',
+  components: {
+    'field-text': FieldText,
+    'field-checkbox': FieldCheckbox,
+  },
+  model: {
+    prop: 'user',
+    event: 'update',
+  },
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="user-profile">
     <div v-if="user">
-      <h2 class="subtitle">{{ userFullName }}</h2>
-      <form>
-        <div class="field">
-          <label class="label">First name:</label>
-          <div class="control">
-            <input class="input" :value="user.firstName" />
-          </div>
-        </div>
-        <div class="field">
-          <label class="label">Last name:</label>
-          <div class="control">
-            <input class="input" :value="user.lastName" />
-          </div>
-        </div>
-      </form>
+      <h2 class="subtitle">
+        {{ userFullName }}
+      </h2>
+      <user-form v-model="user" />
     </div>
     <div v-else>
       <p>Loading...</p>
@@ -24,7 +13,12 @@
 </template>
 
 <script>
+import UserForm from '@/components/UserForm.vue';
+
 export default {
+  components: {
+    'user-form': UserForm,
+  },
   props: {
     user: {
       type: Object,
