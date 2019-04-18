2823316bd158f44789dbff293bdaf2d61e27eaea
evpozdniakov <evpozdniakov@gmail.com>
2019-04-18 09:10:18 -0400

    Add more columns to user table.

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,12 +1,25 @@
 <template>
   <table class="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Registered</th>
+        <th>Active</th>
+        <th />
+      </tr>
+    </thead>
     <tbody>
       <tr
         v-for="user in users"
         :key="user.id"
       >
-        <td>{{ user.firstName }}</td>
-        <td>({{ user.lastName }})</td>
+        <td>{{ getUserFullName(user) }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.phone }}</td>
+        <td>{{ user.registered }}</td>
+        <td>{{ user.isActive }}</td>
         <td>
           <router-link
             class="button is-small is-outlined"
@@ -29,6 +42,9 @@ export default {
     },
   },
   methods: {
+    getUserFullName(user) {
+      return `${user.firstName} ${user.lastName}`
+    },
     linkToUserAccount(id) {
       return `/account/${id}`
     },
